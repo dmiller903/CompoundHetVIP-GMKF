268fa16b0f52f45d9ca14353871778a999f6c616
Dustin Miller <dmiller903@gmail.com>
2020-03-09 18:18:12 -0600

    add num cores argument

--- scripts/gemini_load.py
+++ scripts/gemini_load.py
@@ -15,13 +15,14 @@ if not os.path.exists("/usr/local/share/gemini/gemini_data/hg19.vista.enhancers.
 pathToFiles = argv[1]
 if pathToFiles.endswith("/"):
     pathToFiles = pathToFiles[0:-1]
+numCores = argv[2]
 
 # Get disease name based on path
 diseaseName = chromosome = re.findall(r"([\w\-_]+)\/", pathToFiles)[0]
 
 # Load annotated file into a GEMINI database
 os.system(f"gemini load -v {pathToFiles}/{diseaseName}_phased_samples_annotated.vcf \
--p {pathToFiles}/{diseaseName}.fam -t snpEff --cores 42 {pathToFiles}/{diseaseName}_phased_samples_annotated_cadd.db")
+-p {pathToFiles}/{diseaseName}.fam -t snpEff --cores {numCores} {pathToFiles}/{diseaseName}_phased_samples_annotated_cadd.db")
 
 # Print output information
 timeElapsedMinutes = round((time.time()-startTime) / 60, 2)
